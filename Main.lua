2a0cb7d399989e82aaa51b5232724cfc8db74a79
liamalexfarmer <liamalexfarmer@gmail.com>
2023-09-15 13:41:53 -0400

    added parralax scrolling

--- Main.lua
+++ Main.lua
@@ -7,7 +7,16 @@ VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
 local background = love.graphics.newImage('sprites/background.png')
+local backgroundScroll = 0
+local BACKGROUND_LOOPING_POINT = 413
+
 local ground = love.graphics.newImage('sprites/ground.png')
+local groundScroll = 0
+
+local SCROLL_SPEED = 60
+local backgroundscrollSpeed = SCROLL_SPEED/2
+local groundscrollSpeed = SCROLL_SPEED
+
 
 function love.load()
 	love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -30,9 +39,18 @@ function love.keypressed(key)
 	end
 end
 
+function love.update(dt)
+	backgroundScroll = (backgroundScroll + backgroundscrollSpeed * dt)
+	% BACKGROUND_LOOPING_POINT
+
+	groundScroll = (groundScroll + groundscrollSpeed * dt)
+	% VIRTUAL_WIDTH
+end
+
+
 function love.draw()
 	push:start()
-	love.graphics.draw(background, 0, 0)
-	love.graphics.draw(ground, 0, VIRTUAL_HEIGHT-16)
+	love.graphics.draw(background, -backgroundScroll, 0)
+	love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT-16)
 	push:finish()
 end
