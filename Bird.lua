faf28aa7d419be10e24256a8ba692251649bbbc3
liamalexfarmer <liamalexfarmer@gmail.com>
2023-09-15 15:54:12 -0400

    implemented gravity & anti-gravity

--- Bird.lua
+++ Bird.lua
@@ -1,14 +1,40 @@
+--defines the class
 Bird = Class{}
 
+--variable for gravity acceleration
+local GRAVITY = 20
+
+--initialized variables for the bird class
 function Bird:init()
+	--calls upon the image file, and defines (w,h) using image parameters
 	self.image = love.graphics.newImage('sprites/bird.png')
 	self.width = self.image:getWidth()
 	self.height = self.image:getHeight()
 
+
+	--positions the bird in the middle of the screen
 	self.x = (VIRTUAL_WIDTH / 2) - (self.width / 2)
 	self.y = (VIRTUAL_HEIGHT / 2) - (self.height / 2)
+
+	--allows acceleration on the y axis
+	self.dy = 0
 end
 
+--how the bird behaves over time
+function Bird:update(dt)
+	--enables the bird to constantly accelerate over time based on gravity
+	self.dy = self.dy + GRAVITY * dt
+
+	--performs a jump if spacebard was pressed--makes it momentary
+	if love.keyboard.wasPressed('space') then
+		self.dy = -GRAVITY/4
+	end
+
+	--injects our y axis acceleration parameters into the birds y positioning
+	self.y = self.y + self.dy
+end
+
+--renders the bird based on it's defined parameters
 function Bird:render()
 	love.graphics.draw(self.image, self.x, self.y)
 end
--- Main.lua
+++ Main.lua
@@ -1,30 +1,41 @@
+--calling the push engine
 push = require 'push'
-
+--enabling the usage of class
 Class = require 'class'
 
+--requiring the bird class
 require 'Bird'
 
+require 'Pipe'
 
+--setting and scaling dimentions of the game window
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
+
+--setting the background image, and defining the pixel value that separates it's loopability
 local background = love.graphics.newImage('sprites/background.png')
 local backgroundScroll = 0
 local BACKGROUND_LOOPING_POINT = 413
 
+
+--requires no looping point
 local ground = love.graphics.newImage('sprites/ground.png')
 local groundScroll = 0
 
+--creating a parallax effect between the ground and the background
+--using a constant SCROLL_SPEED
 local SCROLL_SPEED = 60
 local backgroundscrollSpeed = SCROLL_SPEED/2
 local groundscrollSpeed = SCROLL_SPEED
 
+--creating a local bird value based on the bird class
 local bird = Bird()
 
-
+--establishing graphis formats, windo titles and screen setup parameters
 function love.load()
 	love.graphics.setDefaultFilter('nearest', 'nearest')
 	love.window.setTitle("No-Cappy Byrd")
@@ -34,33 +45,63 @@ function love.load()
 		fullscreen = false,
 		resizable = true
 	})
+
+	--creating a table of pressed keys
+	love.keyboard.keysPressed = {}
 end
 
+
+--allows window resizing
 function love.resize(w,h)
 	push:resize(w, h)
 end
 
+--
 function love.keypressed(key)
+	love.keyboard.keysPressed[key] = true
+
 	if key == 'escape' then
 		love.event.quit()
 	end
 end
 
+--creates a boolean response based on keys pressed
+function love.keyboard.wasPressed(key)
+	if love.keyboard.keysPressed[key] then
+		return true
+	else
+		return false
+	end
+end
+
+--defines our games behavior over time
 function love.update(dt)
+	--creates a moving X value for the background image that allows it to scroll over time
+	--modulo resets the X value close to 0 for continuous scrolling
 	backgroundScroll = (backgroundScroll + backgroundscrollSpeed * dt)
 	% BACKGROUND_LOOPING_POINT
 
+	--same principle as above but using the virtual width as a mechanism to reset the x point.
 	groundScroll = (groundScroll + groundscrollSpeed * dt)
 	% VIRTUAL_WIDTH
+
+	--executes the update functions defined in bird class
+	bird:update(dt)
+
+	--flushes the keys pressed table on update
+	love.keyboard.keysPressed = {}
 end
 
-
+--graphics drawing functions
 function love.draw()
+	--required for push utilization
 	push:start()
 
+	--calls upon moving variables defined above to enable scrolling
 	love.graphics.draw(background, -backgroundScroll, 0)
 	love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT-16)
 
+	--renders our bird on screen based on class parameters.
 	bird:render()
 
 	push:finish()
